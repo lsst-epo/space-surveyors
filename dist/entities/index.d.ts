e2733990973532d05fc5be4a12e7a183b91fe0b0
Alexandra Goff <agoff@lsst.org>
2022-06-17 11:39:58 -0700

    [F] game structure setup & fix example

---
+++ dist/entities/index.d.ts
@@ -0,0 +1,2 @@
+declare const Entities: {};
+export default Entities;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
-import * as React from 'react';
+import 'regenerator-runtime/runtime.js';
+import React from 'react';
+import { GameEngine } from 'react-game-engine';
+import Entities from '@entities/index';
+import Systems from '@systems/index';
 
-// Delete me
-export const Thing = () => {
-  return <div>the snozzberries taste like snozzberries</div>;
-};
+class SpaceSurveyors extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Test</h1>{' '}
+        <GameEngine
+          entities={Entities}
+          systems={Systems}
+          style={{
+            height: '100vh',
+            backgroundColor: 'peachpuff',
+            overflow: 'hidden',
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export { SpaceSurveyors };
