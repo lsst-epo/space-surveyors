46455d9f629b8113a8044be1cd4e6a52740ee7a9
Alexandra Goff <agoff@lsst.org>
2022-06-17 11:47:39 -0700

    [F] game structure setup & fix example
    * [F] game structure setup & fix example
    
    * [C] remove github build action

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,6 @@
-/// <reference types="react" />
-export declare const Thing: () => JSX.Element;
+import 'regenerator-runtime/runtime.js';
+import React from 'react';
+declare class SpaceSurveyors extends React.Component {
+    render(): JSX.Element;
+}
+export { SpaceSurveyors };
