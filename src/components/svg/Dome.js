c3ce3374449f92e81e003c71e3500b7e8f94de94
Alexandra Goff <agoff@lsst.org>
2022-07-07 12:19:04 -0700

    [C] fix responsive HUD issues

--- src/components/svg/Dome.js
+++ src/components/svg/Dome.js
@@ -1,28 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
-import SVG from 'react-inlinesvg';
-import PropTypes from 'prop-types';
 import DomeSVG from '@assets/svg/dome.svg';
-import { zStack, BREAK_DESKTOP } from '@styles/globalStyle';
+import { zStack } from '@styles/globalStyle';
 
-const Dome = styled(SVG).attrs(() => ({
+/**
+ * this SVG is served from an img so that
+ * useResizeObserver can properly set a ref
+ * on it
+ */
+const Dome = styled.img.attrs(() => ({
   src: DomeSVG,
 }))`
   height: 80%;
   position: absolute;
   z-index: ${zStack.hud};
   user-select: none;
-  ${({ $left }) =>
-    $left ? 'transform: scaleX(-1); right: 90%;' : 'left: 90%;'}
-
-  @media (min-width: ${BREAK_DESKTOP}) {
-    ${({ $left }) =>
-      $left ? 'left: 0' : 'left: 100%; transform: translateX(-100%);'}
-  }
 `;
-
-Dome.propTypes = {
-  $left: PropTypes.bool,
-};
 
 export default Dome;
