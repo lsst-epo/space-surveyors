506bc55e534f00299b4977b7ea877514df26211a
Alexandra Goff <agoff@lsst.org>
2022-07-20 15:11:15 -0700

    [C] add aspect ratio scaling
    For JIRA: "EPO-6608 #IN-REVIEW #comment add aspect ratio scaling"

--- src/components/Camera/Target.js
+++ src/components/Camera/Target.js
@@ -1,16 +1,12 @@
 import styled from 'styled-components';
-import { TARGET_SIZE } from '@constants/index';
 
-const size = TARGET_SIZE;
-const offset = size / 2;
-
-const CameraTarget = styled.div.attrs(({ x, y }) => ({
+const CameraTarget = styled.div.attrs(({ x, y, size }) => ({
   style: {
     left: `${x}%`,
     top: `${y}%`,
+    width: `${size / 5}%`,
   },
 }))`
-  width: ${size}%;
   border-radius: 50%;
   background-color: var(--red);
   opacity: 0.5;
---
+++ src/constants/objects/sizes.ts
@@ -0,0 +1,60 @@
+import { SkyObjectType } from '@shapes/index';
+import { getRandomDecimal, round } from '../../utils';
+
+const OBJECT_SIZE: {
+  [Key in SkyObjectType | 'camera']: {
+    min: number;
+    target?: number;
+    max: number;
+  };
+} = {
+  camera: {
+    min: 15,
+    target: 25,
+    max: 30,
+  },
+  star: {
+    min: 1,
+    max: 3,
+  },
+  asteroid: {
+    min: 1,
+    max: 3,
+  },
+  comet: {
+    min: 1,
+    max: 3,
+  },
+  galaxy: {
+    min: 1,
+    max: 3,
+  },
+  supernova: {
+    min: 1,
+    max: 3,
+  },
+  cloud: {
+    min: 20,
+    max: 40,
+  },
+  airplane: {
+    min: 3,
+    max: 3,
+  },
+};
+
+export const getScaledObjectSize = (
+  type: SkyObjectType | 'camera',
+  aspectRatio: number
+): number => {
+  const { [type]: object } = OBJECT_SIZE;
+  const size: number = object.target
+    ? object.target
+    : getRandomDecimal(object.min, object.max, 1);
+
+  const scaledSize: number = round(size / aspectRatio);
+
+  return object.target
+    ? Math.min(Math.max(scaledSize, object.min), object.max)
+    : Math.max(scaledSize, object.min);
+};
