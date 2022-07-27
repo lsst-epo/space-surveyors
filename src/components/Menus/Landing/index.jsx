4d5b4cf6a3faca288d3224535d9db58c61547422
Alexandra Goff <agoff@lsst.org>
2022-07-27 09:07:41 -0700

    [C] fix missing path camera movement

--- src/components/Menus/Landing/index.jsx
+++ src/components/Menus/Landing/index.jsx
@@ -29,34 +29,13 @@ const LandingMenuTitle = styled.h1`
   font-style: italic;
 `;
 
-const InstructionsList = styled.ol`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  grid-template-rows: auto;
-  grid-gap: 1rem;
-  grid-auto-flow: dense;
-  min-width: 80%;
-  width: 800px;
-  margin-bottom: 1em;
-  max-width: 100%;
-  list-style-position: inside;
-`;
-
-const InstructionsListItem = styled.li`
-  border-radius: 0.5rem;
-  background-color: var(--turquoise70);
-  color: var(--neutral10);
-  font-size: 1.2em;
-  list-style: none;
-  padding: 0.5em 1em;
-
-  > * + * {
-    margin-top: 1em;
-  }
-`;
-
 const IconLegend = styled.ul`
+  color: var(--offWhite);
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
   list-style: none;
+  flex-wrap: wrap;
 `;
 
 const IconItem = styled.li`
@@ -73,6 +52,24 @@ const FundingLogos = styled.img`
   width: 800px;
   margin-top: auto;
   max-width: 100%;
+`;
+
+const Instructions = styled.ul`
+  font-size: 1.5em;
+  width: 800px;
+  max-width: 100%;
+  margin-bottom: 1em;
+  list-style-position: inside;
+
+  & > li {
+    & > * {
+      margin-top: 1em;
+    }
+  }
+
+  & > li + li {
+    margin-top: 1em;
+  }
 `;
 
 const icons = {
@@ -96,22 +93,11 @@ const LandingMenu = ({ onMenuAction }) => {
         <RubinLogo src={rubinLogo} height="110px" width="180px" />
         <LandingMenuTitle>Space Surveyors</LandingMenuTitle>
       </TitleBar>
-      <InstructionsList>
-        <InstructionsListItem>
-          <h2>Scan the night sky</h2>
-          <p>
-            The Vera Rubin Observatory will capture ~10 million objects every
-            night with it's 8.4 meter primary camera. The focal plane of the
-            camera spans 3.5° of the night sky, roughly 7 times as large as the
-            full moon appears.
-          </p>
-          <FocalPlaneVisual />
-          <p>
-            In Space Surveyors you will have{' '}
-            <strong>{convertMsToTime(GAME_TIME)}</strong> before the night is
-            over to survey as many objects as possible.
-          </p>
-          <h3>Look for these objects</h3>
+      <Instructions>
+        <li>
+          In Space Surveyors you will have{' '}
+          <strong>{convertMsToTime(GAME_TIME)}</strong> before the night ends to
+          survey as many objects as possible. Look for these objects:
           <IconLegend>
             {Object.keys(icons).map((i) => (
               <IconItem key={i}>
@@ -122,31 +108,19 @@ const LandingMenu = ({ onMenuAction }) => {
               </IconItem>
             ))}
           </IconLegend>
-        </InstructionsListItem>
-        <InstructionsListItem>
-          <h2>Use Rubin's camera</h2>
-          <p>
-            When the timer starts, click or tap anywhere on the screen to begin
-            moving the telescope's focal plane to a new position. When the focal
-            plane arrives at it's next location, it will begin exposing an image
-            to survey.
-          </p>
+        </li>
+        <li>
+          When the timer starts, click or tap anywhere on the screen to move the
+          telescope to a new position and begin exposing an image.
           <MovementVisual />
-          <p>
-            Every camera exposure will take time so plan your moves carefully,
-            the night only lasts so long!
-          </p>
-        </InstructionsListItem>
-        <InstructionsListItem>
-          <h2>Watch out!</h2>
-          <p>
-            Celestial objects are not the only things in the night sky. Clouds,
-            airplanes, and satellites may disrupt your camera and prevent it
-            from surveying the objects you want.
-          </p>
+        </li>
+        <li>
+          Celestial objects are not the only things in the night sky. Clouds,
+          airplanes, and satellites may disrupt your camera and prevent it from
+          surveying the objects you want.
           <OcclusionVisual />
-        </InstructionsListItem>
-      </InstructionsList>
+        </li>
+      </Instructions>
       <Button onClick={handleGameStart}>Start</Button>
       <FundingLogos src={fundingLogos} />
     </BaseMenu>
