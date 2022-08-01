eb06cbee31009fad0ebf4c49f7d85f5b377c70a9
Alexandra Goff <agoff@lsst.org>
2022-08-01 16:28:10 -0700

    [C] more audio

--- src/entities/audio.ts
+++ src/entities/audio.ts
@@ -1,6 +1,5 @@
 import { Howl } from 'howler';
-// import backgroundAudio from '@assets/audio/rave_digger.webm';
-const backgroundAudio = require('assets/rave_digger.webm');
+import backgroundAudio from '@assets/audio/rave_digger.webm';
 
 export default async () => {
   const background = new Howl({ src: backgroundAudio });
