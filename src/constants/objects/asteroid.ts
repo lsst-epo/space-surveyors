25eb2d8687cff6f58473cec84b35bee0a333262a
Alexandra Goff <agoff@lsst.org>
2022-08-10 12:13:55 -0700

    [F] Score sharing button setup

--- src/constants/objects/asteroid.ts
+++ src/constants/objects/asteroid.ts
@@ -8,7 +8,7 @@ export default {
     max: 2.5,
   },
   spawnEdge: { left: 1, right: 1, top: 3, bottom: 3 },
-  speed: { min: 0.02, target: 0.0225, max: 0.03 },
+  speed: { min: 0.04, target: 0.045, max: 0.05 },
   baseRotation: 0,
   onlyMovesHorizontal: false,
 };
