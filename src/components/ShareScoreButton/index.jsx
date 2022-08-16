1803a2927721dac111457c1a7f226eaddde3cd70
Alexandra Goff <agoff@lsst.org>
2022-08-16 16:01:49 -0700

    [C] roll back detect-collisions

--- src/components/ShareScoreButton/index.jsx
+++ src/components/ShareScoreButton/index.jsx
@@ -1,20 +1,20 @@
-import Button from '@components/Button';
-import React, { useState } from 'react';
-import copy from 'copy-to-clipboard';
+import Button from "@components/Button";
+import React, { useState } from "react";
+import copy from "copy-to-clipboard";
 
 const ShareScoreButton = ({ score, total }) => {
-  const [buttonText, setButtonText] = useState('Share score');
+  const [buttonText, setButtonText] = useState("Share score");
 
   const copyScore = () => {
     const { asteroid, comet, galaxy, supernova, star } = score;
-    const toCopy = `I discovered ${total} objects in Rubin #SpaceSurveyors\r\n\r\n🪨 - ${asteroid} ☄️ - ${comet} 🌌 - ${galaxy} ⭐ - ${star} 💥 - ${supernova}\r\n\r\nhttps://spacesurveyors.app/`;
+    const toCopy = `I discovered ${total} objects in Rubin #SpaceSurveyors\r\n\r\n🪨 - ${asteroid} ☄️ - ${comet} 🌌 - ${galaxy} ⭐ - ${star} 💥 - ${supernova}\r\n\r\n${window.location}`;
     const didCopy = copy(toCopy);
 
     if (didCopy) {
-      setButtonText('Copied!');
+      setButtonText("Copied!");
 
       setTimeout(() => {
-        setButtonText('Share score');
+        setButtonText("Share score");
       }, 3000);
     }
   };
