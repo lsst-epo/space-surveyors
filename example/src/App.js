9bee833ba7ba265f573f2955c90e362bd678ac09
Alexandra Goff <agoff@lsst.org>
2022-09-15 10:01:36 -0700

    [F] add embedded example to space surveyors

--- example/src/App.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import SpaceSurveyors from '../../dist';
-import 'modern-css-reset/dist/reset.min.css';
-import './App.css';
-
-export const App = () => <SpaceSurveyors></SpaceSurveyors>;
---
+++ example/src/embedded.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "modern-css-reset/dist/reset.min.css";
+import Embed from "./embed";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Embed />);
