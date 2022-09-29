9fbf48a507cd97c000b839f1f2b9050e963409b7
Alexandra Goff <agoff@lsst.org>
2022-09-29 16:31:23 -0700

    [C] add Spanish translations

--- src/components/ShareScoreButton/index.jsx
+++ src/components/ShareScoreButton/index.jsx
@@ -5,9 +5,7 @@ import { useTranslation } from "react-i18next";
 
 const ShareScoreButton = ({ score, total }) => {
   const { t } = useTranslation();
-  const [buttonText, setButtonText] = useState(
-    t("generics.actions.share_score")
-  );
+  const [buttonKey, setButtonKey] = useState(t("generics.actions.share_score"));
 
   const copyScore = () => {
     const { asteroid, comet, galaxy, supernova, star } = score;
@@ -23,15 +21,15 @@ const ShareScoreButton = ({ score, total }) => {
     const didCopy = copy(toCopy);
 
     if (didCopy) {
-      setButtonText(t("generics.actions.copied"));
+      setButtonKey("generics.actions.copied");
 
       setTimeout(() => {
-        setButtonText(t("generics.actions.share_score"));
+        setButtonKey("generics.actions.share_score");
       }, 3000);
     }
   };
 
-  return <Button onClick={copyScore}>{buttonText}</Button>;
+  return <Button onClick={copyScore}>{t(buttonKey)}</Button>;
 };
 
 export default ShareScoreButton;
